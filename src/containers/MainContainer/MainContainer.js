07ea2f59fe84b9ada2f882ae320d33af9c3d4490
CallumGrindle <callumgrindle@gmail.com>
2019-11-21 14:49:47 +0000

    content compoennt added

--- src/containers/MainContainer/MainContainer.js
+++ src/containers/MainContainer/MainContainer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Nav from '../../components/Nav/Nav.js'
 import Carousel from '../../components/carousel/carousel.js'
+import Content from '../ContentContainer/Content.js'
 import './MainContainer.css'
 
 const MainContainer = (props) => {
@@ -8,6 +9,7 @@ const MainContainer = (props) => {
     <section>
       <Nav />
       <Carousel className='carousel'/>
+      <Content />
     </section>
   )
 }
